e943564ba367b2d14693731e8587de3b012bcccf
unknown <dudusabag73@gmail.com>
2019-08-11 20:37:22 +0300

    Updates

---
+++ precache-manifest.43eb288ad2bbc3817909dfdf8c9ddb8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobofriendsReactApp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "88a1f1175ef72d0b5fc9",
+    "url": "/RobofriendsReactApp/static/js/runtime~main.88a1f117.js"
+  },
+  {
+    "revision": "7e2407fb585516173752",
+    "url": "/RobofriendsReactApp/static/js/main.7e2407fb.chunk.js"
+  },
+  {
+    "revision": "1f3a8616b03ba438e70c",
+    "url": "/RobofriendsReactApp/static/js/1.1f3a8616.chunk.js"
+  },
+  {
+    "revision": "7e2407fb585516173752",
+    "url": "/RobofriendsReactApp/static/css/main.dc53b429.chunk.css"
+  },
+  {
+    "revision": "1f3a8616b03ba438e70c",
+    "url": "/RobofriendsReactApp/static/css/1.1cbf3a6e.chunk.css"
+  },
+  {
+    "revision": "e851b948ab5f86e135025231a749e7a3",
+    "url": "/RobofriendsReactApp/index.html"
+  }
+];
